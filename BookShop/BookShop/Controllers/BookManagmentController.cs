30a1f7cdde29f6617ee9ec4a3d8ff2519fbb8a70
sajad440 <sajad.akhlaghi004@gmail.com>
2025-02-18 13:20:05 +0330

    feature : add book. and change Book page

---
+++ BookShop/BookShop/Controllers/BookManagmentController.cs
@@ -0,0 +1,58 @@
+﻿using BookShopDataAccess;
+using bookShopModel;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BookShop.Controllers
+{
+    public class BookManagmentController : Controller
+    {
+        private BookDbContext _dbContext;
+
+        public BookManagmentController(BookDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Book book1)
+        {
+            if (book1 == null)
+            {
+                TempData["Fail"] = "invalid input";
+                return RedirectToAction("Add");
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _dbContext.books.Add(book1);
+                    _dbContext.SaveChanges();
+                    TempData["Success"] = "Book add successful";
+                    return RedirectToAction("Add");
+                }
+                catch (Exception ex)
+                {
+                    TempData["Fail"] = "Book cannot  add";
+                    Console.WriteLine(ex.Message); 
+                }
+            }
+            else
+            {
+                TempData["Fail"] = "Book not valid ";
+            }
+
+            return RedirectToAction("Add");
+        }
+
+    }
+}
--- BookShop/bookShopModel/Book.cs
+++ BookShop/bookShopModel/Book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ namespace bookShopModel
     public class Book
     {
         [Key]
-        public int Id;
+        public int Id { get; set; }
         [Required]
         public string Name { get; set; }
         [Required]
